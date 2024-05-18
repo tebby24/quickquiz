0fe6d97a291a36b34c756a026d6fb2a91d8633ca
tebby24 <tvgonyea@iu.edu>
2024-05-18 12:12:02 -0400

    added undo shortcut option

--- docs/js/quiz.js
+++ docs/js/quiz.js
@@ -1,9 +1,19 @@
+class QuizState {
+    constructor(terms, failed, passed, curr) {
+        this.terms = terms;
+        this.failed = failed;
+        this.passed = passed;
+        this.curr = curr;
+    }
+}
+
 class Quiz {
     // a Quiz is a set of questions
     constructor() {
         this.terms = [];
         this.failed = [];
         this.passed = [];
+        this.stateHistory = [];
         this.curr;
     }
 
@@ -11,7 +21,7 @@ class Quiz {
         this.terms.push(term);
     }
 
-    getNextTerm() {
+    setNextTerm() {
         if (this.terms.length == 0) {
             if (this.failed.length == 0) {
                 // quiz over
@@ -26,14 +36,32 @@ class Quiz {
         this.curr = this.terms[randomIndex];
     }
 
+    saveState() {
+        this.stateHistory.push(new QuizState(this.terms, this.failed, this.passed, this.curr));
+    }
+
     pass() {
+        this.saveState();
         this.terms = this.terms.filter(term => term !== this.curr);
         this.passed.push(this.curr);
+        this.setNextTerm()
     }
 
     fail() {
+        this.saveState()
         this.terms = this.terms.filter(term => term !== this.curr);
         this.failed.push(this.curr);
+        this.setNextTerm()
+    }
+
+    undo() {
+        if (this.stateHistory.length > 0) {
+            const lastState = this.stateHistory.pop();
+            this.terms = lastState.terms;
+            this.failed = lastState.failed;
+            this.passed = lastState.passed;
+            this.curr = lastState.curr;
+        }
     }
 }
 
@@ -48,38 +76,45 @@ const terms = userInput.split('\n').filter(term => term.trim() !== '');
 terms.forEach((term) => {
     quiz.addQuestion(term);
 })
+quiz.setNextTerm()
 
-function showNextTerm() {
-    quiz.getNextTerm()
+function updateTerm() {
     if (quiz.curr == null) {
-        window.location.href = 'congratulations.html'    
+        window.location.href = 'congratulations.html'
     }
     else {
-        termDisplay.innerText = quiz.curr;        
+        termDisplay.innerText = quiz.curr;
     }
 }
-showNextTerm()
 
 passButton.addEventListener("click", () => {
     quiz.pass()
-    showNextTerm()
+    updateTerm()
 })
 
 failButton.addEventListener("click", () => {
     quiz.fail()
-    showNextTerm()
+    updateTerm()
 })
 
 passTermShortcut = localStorage.getItem('passTermShortcut')
 failTermShortcut = localStorage.getItem('failTermShortcut')
+undoShortcut = localStorage.getItem('undoShortcut')
 
 
 document.addEventListener('keydown', function(event) {
     if (event.key === passTermShortcut) {
         quiz.pass();
-        showNextTerm();
+        updateTerm()
     } else if (event.key === failTermShortcut) {
         quiz.fail();
-        showNextTerm();
+        updateTerm();
+    } else if (event.key === undoShortcut) {
+        quiz.undo();
+        updateTerm();
     }
+
 });
+
+// start the quiz
+updateTerm()
--- docs/js/settings.js
+++ docs/js/settings.js
@@ -1,8 +1,25 @@
+function shortcutsAreUnique() {
+    var passTermShortcut = document.getElementById("passTermShortcut").value;
+    var failTermShortcut = document.getElementById("failTermShortcut").value;
+    var undoShortcut = document.getElementById("undoShortcut").value;
+    shortcutsList = [passTermShortcut, failTermShortcut, undoShortcut];     
+    uniqueShortcuts = new Set(shortcutsList);
+    if(shortcutsList.length !== uniqueShortcuts.size){
+        return false;
+    }
+    return true;
+}
+
 function submitForm() {
+    if(!shortcutsAreUnique()){
+        alert("Shortcuts must be unique");
+        return;
+    }
+
     localStorage.setItem("colorTheme", getColorTheme(document.getElementById("colorThemeSelect").value))
     localStorage.setItem("passTermShortcut", document.getElementById("passTermShortcut").value)
     localStorage.setItem("failTermShortcut", document.getElementById("failTermShortcut").value)
-    console.log("back to home page")
+    localStorage.setItem("undoShortcut", document.getElementById("undoShortcut").value) 
     window.location.href = 'index.html';
 }
 
@@ -20,3 +37,4 @@ else {
 
 document.getElementById('passTermShortcut').value = localStorage.getItem('passTermShortcut')
 document.getElementById('failTermShortcut').value = localStorage.getItem('failTermShortcut')
+document.getElementById('undoShortcut').value = localStorage.getItem('undoShortcut')
